9e2ad6c88213629857f7479e1b157dec0c0c8b40
calinevaliubovi <calinevaliubovi2@gmail.com>
2024-11-03 13:59:18 +0200

    feat:add interactiv for button

--- src/Components/CardItem/CardItem.jsx
+++ src/Components/CardItem/CardItem.jsx
@@ -1,15 +1,20 @@
 
 import styles from "./cardItem.module.css"
+import { useState } from "react";
 
 export default function CardItem({ name, price, internet, text, color }){
+   const [flag,setFlag] = useState(false);
+
     return(
-        <div className={styles[color + `Card`]}>
+        <div className={flag? styles[color + `Card`]:""}>
+        {/* // <div className={'${styles.color} ${flag? "styles.scale": ""' }> */}
             <div className={styles[color]}>
                 <h2 className={styles.h2}>{name}</h2>
                 <p className = {styles.price}>{price}</p>
             </div>
             <p className = {styles.internet}>{internet}</p>
             <p className = {styles.text}>{text}</p>
+            <button className={styles.button} onClick={() => setFlag(!flag)}>Выбери тариф</button>
         </div>
     );
 }
